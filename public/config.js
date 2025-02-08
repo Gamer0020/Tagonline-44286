4b50595e843d84fae8082ba901b0ca881d2f4d8a
Gamer0020 <128790162+Gamer0020@users.noreply.github.com>
2025-02-08 18:46:48 +0100

    Added folder game and created config.

---
+++ public/config.js
@@ -0,0 +1,10 @@
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+    apiKey: "AIzaSyDViTqc4QOkpqX0wAdBV0rLlKt2iMK3bz8",
+    authDomain: "tagonline-44286.firebaseapp.com",
+    databaseURL: "https://tagonline-44286-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "tagonline-44286",
+    storageBucket: "tagonline-44286.firebasestorage.app",
+    messagingSenderId: "261231723787",
+    appId: "1:261231723787:web:a2f121380bfd3b8b7b4e0e"
+  };
--- public/game/game.js
+++ public/game/game.js
@@ -6,18 +6,8 @@ import { getDatabase, ref, set, get, push, onValue, onDisconnect, remove, onChil
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDViTqc4QOkpqX0wAdBV0rLlKt2iMK3bz8",
-  authDomain: "tagonline-44286.firebaseapp.com",
-  databaseURL: "https://tagonline-44286-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "tagonline-44286",
-  storageBucket: "tagonline-44286.firebasestorage.app",
-  messagingSenderId: "261231723787",
-  appId: "1:261231723787:web:a2f121380bfd3b8b7b4e0e"
-};
-
 // Initialize Firebase
+import { firebaseConfig } from "../config.js";
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
